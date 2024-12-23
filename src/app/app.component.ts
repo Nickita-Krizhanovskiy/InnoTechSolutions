c33fde93c1662ba7e9eba67761e8dd528eebbba6
lans1q <skweezyyy@mail.ru>
2024-12-23 21:13:20 +0300

    Add pipe's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { LowerCasePipe } from '@angular/common';
+
+import { LowerCasePipe, UpperCasePipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { pipe } from 'rxjs';
 import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyCalculatorComponent],
+  imports: [MyCalculatorComponent, RouterOutlet, LowerCasePipe, UpperCasePipe, DatePipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  title = Date();
+}
