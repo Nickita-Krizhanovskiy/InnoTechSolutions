80b572267f39fd489421373a4f434a3ee338d776
lans1q <skweezyyy@mail.ru>
2025-01-09 22:43:03 +0300

    Add back and route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 import { LowerCasePipe, UpperCasePipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
@@ -9,10 +10,11 @@ import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MyCalculatorComponent, RouterOutlet, LowerCasePipe, UpperCasePipe, DatePipe],
+  imports: [MyCalculatorComponent, RouterOutlet, LowerCasePipe, UpperCasePipe, DatePipe, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = Date();
+  backgroundImage = 'assets/back.png'
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
+import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'login', component: LoginPageComponent }
+];
